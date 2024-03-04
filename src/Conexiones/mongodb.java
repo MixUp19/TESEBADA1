b9d1af830dc34c1f29c229e84b620d220e791a39
cindyyomar <57692465+cindyyomar@users.noreply.github.com>
2024-03-03 21:32:07 -0700

    Update mongodb.java

--- src/Conexiones/mongodb.java
+++ src/Conexiones/mongodb.java
@@ -3,10 +3,11 @@ package src.Conexiones;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
-public mongodb{
-
+public class mongodb{
+public mongodb(String databaseName){
 String connectionString = "mongodb://localhost:27017";
 
         MongoClient mongoClient = new MongoClient(new MongoClientURI(connectionString));
-        MongoDatabase database = mongoClient.getDatabase("YourDatabaseName");
+        MongoDatabase database = mongoClient.getDatabase(databaseName);
+}
 }
